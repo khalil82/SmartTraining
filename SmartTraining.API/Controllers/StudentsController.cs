cfcbd179d2aa6f879e337530a54ed2c4ea8130a3
khalil82 <mohamed.khalil82@users.noreply.github.com>
2018-09-22 02:54:28 +0200

    Students List, and Adding Logo
    Students List, and Adding Logo

---
+++ SmartTraining.API/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using SmartTraining.Business.Handlers.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartTraining.API.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IStudentHandler _studentHandler;
+
+        public StudentsController(IStudentHandler studentHandler)
+        {
+            _studentHandler = studentHandler;
+        }
+        public ActionResult Student(int id)
+        {
+            var course = _studentHandler.GetStudentById(id);
+            return View(course);
+        }
+
+        public ActionResult All()
+        {
+            var allStudents = _studentHandler.GetAll();
+            return View(allStudents);
+        }
+
+
+    }
+}
--- SmartTraining.Business.Handlers/Student/IStudentHandler.cs
+++ SmartTraining.Business.Handlers/Student/IStudentHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace SmartTraining.Business.Handlers.Student
+﻿using System.Collections.Generic;
+using SmartTraining.Business.DTO.Courses;
+using SmartTraining.Business.DTO.Students;
+
+namespace SmartTraining.Business.Handlers.Student
 {
     public interface IStudentHandler
     {
+        StudentDto GetStudentById(int id);
+        List<CourseDto> GetCourseStudents(int id);
+        List<StudentDto> GetAll();
+        bool EditStudent(StudentDto studentDto);
+        bool Create(StudentDto studentDto);
+        bool Delete(int id);
     }
 }
